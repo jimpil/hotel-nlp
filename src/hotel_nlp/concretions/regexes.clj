540d4ffbf6ecc8a051cf715d24e9d5e2761ba799
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-03-11 21:36:37 +0000

    added abbreviation extraction regex

--- src/hotel_nlp/concretions/regexes.clj
+++ src/hotel_nlp/concretions/regexes.clj
@@ -10,8 +10,15 @@
 )
 
 
-(def sentence-segmentation-regex  #"(?<=[.!?]|[.!?][\\'\"])(?<!e\.g\.|i\.e\.|vs\.|p\.m\.|a\.m\.|Mr\.|Mrs\.|Ms\.|St\.|Fig\.|fig\.|Jr\.|Dr\.|Prof\.|Sr\.|\s[A-Z]\.)\s+")
-(def token-regex #"[\w\d/]+|[\-\,\.\?\!\(\)]")
-(def _LEAD  #"^[aeiouy]*(?:qu|[bcdfghjklmnpqrstvwxz])+")    ;;match 0 or more vowels + 1 or more consonants at the start of the word 
-(def _INNER #"\B[aeiouy]+(?:qu|[bcdfghjklmnpqrstvwxz])+\B") ;;match 1 or more vowels + 1 or more consonants inside a word 
-(def _TRAIL #"[aeiouy]+(?:qu|[bcdfghjklmnpqrstvwxz])+$")    ;;match 1 or more vowels + 0 or more consonants at the end of a word 
+(def sentence-segmentation-regex  "Regular expression capable of identifying sentence boundaries."  
+	#"(?<=[.!?]|[.!?][\\'\"])(?<!e\.g\.|i\.e\.|vs\.|p\.m\.|a\.m\.|Mr\.|Mrs\.|Ms\.|St\.|Fig\.|fig\.|Jr\.|Dr\.|Prof\.|Sr\.|\s[A-Z]\.)\s+")
+(def token-regex "Regular expression capable of identifying word boundaries." 
+	#"[\w\d/]+|[\-\,\.\?\!\(\)]")
+(def abbreviation-regex "Regular expression capable of identifying abbreviations (NOT acronyms!) contained in parentheses (at least 2 characters)." 
+      #"(?i)[a-zA-Z0-9-]*\s?\(\w{2,}\)")       				    ;;match any word followed (possibly) by a /space, followed by non-empty parentheses
+(def _LEAD  "Regular expression that matches 0 or more vowels + 1 or more consonants at the start of the word."
+   #"^[aeiouy]*(?:qu|[bcdfghjklmnpqrstvwxz])+")    
+(def _INNER  "Regular expression that matches 1 or more vowels + 1 or more consonants inside a word."
+   #"\B[aeiouy]+(?:qu|[bcdfghjklmnpqrstvwxz])+\B") 
+(def _TRAIL  "Regular expression that matches 1 or more vowels + 0 or more consonants at the end of a word."
+  #"[aeiouy]+(?:qu|[bcdfghjklmnpqrstvwxz])+$") 
