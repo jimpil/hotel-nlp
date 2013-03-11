2712d41cd3dd3c64f797c6da4b8bb9f6ec001dcc
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-03-11 21:37:12 +0000

    reduntand code removed

--- src/hotel_nlp/concretions/artefacts.clj
+++ src/hotel_nlp/concretions/artefacts.clj
@@ -1,8 +1,9 @@
 (ns hotel_nlp.concretions.artefacts
    (:require  [hotel_nlp.helper :as help]
+   	          [hotel_nlp.concretions.regexes :as re]
               [hotel_nlp.concretions.models]
    )
-   (:import [hotel_nlp.concretions.models RE-Tokenizer RE-Segmenter RE-Combiner PorterStemmer Workflow Ngrams LevenshteinDistance 
+   (:import [hotel_nlp.concretions.models RE-Tokenizer RE-Segmenter RE-Combiner RE-Abbrv PorterStemmer Workflow Ngrams LevenshteinDistance 
                                       MapReduceMapper ForkJoinMapper PoolMapper SemiLazyMapper]
    
    )
@@ -10,10 +11,8 @@
 
 
 
-(def token-regex #"[\w\d/]+|[\-\,\.\?\!\(\)]")
-(def sentence-regex  #"(?<=[.!?]|[.!?][\\'\"])(?<!e\.g\.|i\.e\.|vs\.|p\.m\.|a\.m\.|Mr\.|Mrs\.|Ms\.|St\.|Fig\.|fig\.|Jr\.|Dr\.|Prof\.|Sr\.|\s[A-Z]\.)\s+")
-(def reg-tok (RE-Tokenizer. token-regex :string :string-seq))
-(def reg-seg (RE-Segmenter. sentence-regex :string :string-seq))
+(def reg-tok (RE-Tokenizer. re/token-regex :string :string-seq))
+(def reg-seg (RE-Segmenter. re/sentence-segmentation-regex :string :string-seq))
 (def stick   (RE-Combiner. :string :string-seq))
 (def stemmer (PorterStemmer. "english" :string :string))
 (def n-grams  (Ngrams. 2 :string :string))
@@ -24,5 +23,6 @@
 (def mr-mapper (MapReduceMapper. 100))
 (def brown-nltk-pos-probs (help/read-resource "corpora-train/BROWN-NLTK/brown-probs.txt"))
 (def pannotator-pluggable-sent-splitter  (Workflow. [reg-seg stick]))
+(def abbreviation-extractor (RE-Abbrv. re/abbreviation-regex :string :map))
 
 
