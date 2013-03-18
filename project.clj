2936537f2cfe8bd23bd3965de49b971c0a347122
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-03-18 22:29:45 +0000

    contrib-dependency broken

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories [["bioinformatics-all" "http://bioinformatics.ua.pt/maven/content/groups/public"]]          
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 ;[org.clojure/clojure-contrib "1.2.0"]
  		 [org.clojure/data.zip "0.1.1"]
  		 [org.clojure/tools.nrepl "0.2.2"]
  		 ;[weissjeffm/clojure.prxml "1.3.0-SNAPSHOT"]
