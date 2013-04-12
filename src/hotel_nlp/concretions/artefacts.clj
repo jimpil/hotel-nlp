65b246140c2d79054b4b89a1e8283a2cde36f397
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-04-12 09:39:42 +0100

    2 more artefacts

--- src/hotel_nlp/concretions/artefacts.clj
+++ src/hotel_nlp/concretions/artefacts.clj
@@ -15,14 +15,14 @@
 (def reg-seg (RE-Segmenter. re/sentence-segmentation-regex :string :string-seq))
 (def stick   (RE-Combiner. :string :string-seq))
 (def stemmer (PorterStemmer. "english" :string :string))
-(def n-grams  (Ngrams. 2 :string :string))
+(def bigrams  (Ngrams. 2 :string :string))
 (def MR-mapper  (MapReduceMapper. 5))
 (def main (Workflow. [reg-seg reg-tok stemmer]))
 (def alt  (Workflow. [reg-tok stemmer]))
 (def levenshtein (LevenshteinDistance. :strings :number))
 (def mr-mapper (MapReduceMapper. 100))
 (def brown-nltk-pos-probs (help/read-resource "corpora-train/BROWN-NLTK/brown-probs.txt"))
-(def pannotator-pluggable-sent-splitter  (Workflow. [reg-seg stick]))
+(def pannotator-pluggable-sent-splitter  (Workflow. [reg-seg stick])) ;;replace reg-seg with your own sentene splitter
 (def abbreviation-extractor (RE-Abbrv. [re/re-abbreviation-paren re/re-term-paren] :string :map))
 
 
