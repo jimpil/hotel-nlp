ecf50f9c93d555f51fea64e4e82fcfbdb5d07115
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-05-21 00:38:54 +0100

    cosmetic changes

--- src/hotel_nlp/externals/UIMAProxy.java
+++ src/hotel_nlp/externals/UIMAProxy.java
@@ -8,47 +8,44 @@ import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.UimaContext;
 import org.uimafit.descriptor.ConfigurationParameter;
-//import clojure.lang.DynamicClassLoader;
 import clojure.lang.IFn;
 
 public class UIMAProxy extends JCasAnnotator_ImplBase{
 
-  private UimaContext context;
-  public static final Map<String, Object> resultMap = new HashMap<String, Object>();
+  public static final Map<String, Object> resultMap = new HashMap<String, Object>(); //for quick testing
   public static final String PARAM_POSTFN = "postfn-parameter";
-  public static final String PARAM_ANNFN = "annfn-parameter";
-  public static final String PARAM_EXTFN = "extfn-parameter";
-  public ClassLoader dcl = null; 
+  public static final String PARAM_ANNFN =  "annfn-parameter";
+  public static final String PARAM_EXTFN =  "extfn-parameter";
+  //private ClassLoader dcl = null; 
 
-  @ConfigurationParameter(name = PARAM_POSTFN) //the namespace string
-  private String postfn;
+  @ConfigurationParameter(name = PARAM_POSTFN) //the post-fn string
+  private Class postfn;
 
-  @ConfigurationParameter(name = PARAM_EXTFN) //the input-extractor-fn-class string
-  private String extfn;
+  @ConfigurationParameter(name = PARAM_EXTFN) //the input-extractor-fn string
+  private Class extfn;
 
-  @ConfigurationParameter(name = PARAM_ANNFN) //the annotator-fn-class string
-  private String annfn;
+  @ConfigurationParameter(name = PARAM_ANNFN) //the annotator-fn string
+  private Class annfn;
 
  
   @Override
   public void initialize(final UimaContext context) throws ResourceInitializationException {
     super.initialize(context);
-    this.context = context;
-    dcl = Thread.currentThread().getContextClassLoader();  //should return Clojure's dynamic-classloader
+  //dcl = Thread.currentThread().getContextClassLoader();  //should return Clojure's dynamic-classloader
   }    
 
 
   @Override 
   public void process(JCas aJCas) throws AnalysisEngineProcessException{
 
-      try{ doActualWork(extfn, annfn, postfn, context, aJCas); }
+      try{ doActualWork(extfn, annfn, postfn, getContext(), aJCas); }
       catch (Exception e){
            throw new AnalysisEngineProcessException(e);
         }
         
   }
   //helper method to do all the interop stuff
-   private void doActualWork (String strextfn, String strannfn, String strpostfn, UimaContext context, JCas jcas){
+   private void doActualWork (Class strextfn, Class strannfn, Class strpostfn, UimaContext context, JCas jcas){
     IFn extfn = null; 
     IFn annfn = null;
     IFn postfn = null;
@@ -56,13 +53,16 @@ public class UIMAProxy extends JCasAnnotator_ImplBase{
     
 
     try{  
-        extfn =  (IFn)Class.forName(strextfn,  true, dcl).newInstance();
-        postfn = (IFn)Class.forName(strpostfn, true, dcl).newInstance();     
+        //extfn =  (IFn)Class.forName(strextfn,  true, dcl).newInstance();
+        //postfn = (IFn)Class.forName(strpostfn, true, dcl).newInstance(); 
+        extfn =  (IFn)strextfn.newInstance();   
+        postfn = (IFn)strpostfn.newInstance();   
     }
-    catch (Exception e) {System.err.println("Exception ignored! extractor/post fns remain null...hopefully the annotator can handle extracting from and writing to the CAS.");} 
+    catch (Exception e) 
+     {System.err.println("Exception ignored! extractor/post fns remain null...hopefully the annotator can handle extracting from and writing to the CAS.");} 
 
-
-     try{ annfn =  (IFn)Class.forName(strannfn,  true, dcl).newInstance(); } //this is the important one - cannot be null
+     //annfn = (IFn)Class.forName(strannfn, true, dcl).newInstance();
+     try{ annfn = (IFn)strannfn.newInstance(); } //this is the important one - cannot be null
      catch (Exception e) {
       throw new RuntimeException(e);
     }
@@ -78,7 +78,7 @@ public class UIMAProxy extends JCasAnnotator_ImplBase{
    resultMap.put("result", result); //for testing
 
    if (extfn != null)
-   postfn.invoke(jcas, result, trueInput);
+     postfn.invoke(jcas, result, trueInput);
 
   }
 }
