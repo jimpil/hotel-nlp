3fcc2c32dcf00b9a07ccaadb522e4fdcdb0ff1fd
Dimitris <dimitris@dimitris-OptiPlex-745.(none)>
2013-06-04 15:53:12 +0100

    added nulalry constructor so it can be used reflectively

--- src/hotel_nlp/externals/RelationEdge.java
+++ src/hotel_nlp/externals/RelationEdge.java
@@ -5,8 +5,16 @@ public final class RelationEdge<V> extends DefaultEdge {
     private final V vertex1;
     private final V vertex2;
     private final String label;
+    
+    public RelationEdge(){
+    super();
+    vertex1 = null;
+    vertex2 = null;
+    label   = "default";
+    }
 
     public RelationEdge(V v1, V v2, String label) {
+        super();
         vertex1 = v1;
         vertex2 = v2;
         this.label = label;
