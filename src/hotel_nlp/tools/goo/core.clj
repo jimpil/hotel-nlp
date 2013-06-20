4983d7a02d39be095cb4eb33b7fab8495dcb2fda
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-06-20 14:07:21 +0100

    got rid of reflection

--- src/hotel_nlp/tools/goo/core.clj
+++ src/hotel_nlp/tools/goo/core.clj
@@ -1,9 +1,11 @@
 (ns hotel_nlp.tools.goo.core
-(:import  (java.util Properties) 
-          (java.net URL URLEncoder)
+(:require [hotel_nlp.helper :as help])
+(:import   (java.net URL URLEncoder)
            javax.mail.internet.MimeMessage
            (javax.mail.internet MimeMessage InternetAddress)
            (javax.mail Session Transport Authenticator PasswordAuthentication Message$RecipientType)))
+           
+(set! *warn-on-reflection* true)           
 
 (def google-search-url "http://www.google.com/search?q=")
 (def ^:dynamic *user-agent* "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.22 (KHTML, like Gecko) Chrome/25.0.1364.172")
@@ -25,12 +27,11 @@
   (let [auth (proxy [Authenticator] []
                (getPasswordAuthentication []
                  (PasswordAuthentication. from password)))          
-        props (doto (Properties.)
-                (.putAll {"mail.smtp.user" from
-                          "mail.smtp.host" host
-                          "mail.smtp.starttls.enable" (if ssl? "true" "false")
-                          "mail.smtp.auth" "true"
-                          "mail.smtp.port" (str port)}))
+        props (help/map->properties {"mail.smtp.user" from
+                                     "mail.smtp.host" host
+                         	     "mail.smtp.starttls.enable" (if ssl? "true" "false")
+                                     "mail.smtp.auth" "true"
+                                     "mail.smtp.port" (str port)})
         session (Session/getInstance props auth)
         msg (doto (MimeMessage. session)
               (.setText text)
