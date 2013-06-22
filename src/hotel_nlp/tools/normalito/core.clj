3dc51bad6c0ea65a4ccbcab7f76c197b8281e490
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-06-22 14:09:12 +0100

    added more transformers and cleaned up the api

--- src/hotel_nlp/tools/normalito/core.clj
+++ src/hotel_nlp/tools/normalito/core.clj
@@ -1,5 +1,6 @@
 (ns hotel_nlp.tools.normalito.core
    (:require ;[clojure.pprint :refer [pprint]]
+             [clojure.core.memoize :as memo]
              [hotel_nlp.helper    :as help] 
              [clojure.core.reducers :as r]) )
 ;-----------------------------------------------------------------------------------------------------------------------------------             
@@ -25,59 +26,47 @@ java.util.List ;;if this fires, we're dealing with a Java list-like collection -
 (normalise [this transform]
 (if (instance? java.util.Collection (first this))
 (mapv #(normalise % transform) this)
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
   (reduce 
     (fn [^java.util.List l x] 
      (doto l 
-      (.add (normalise x #(transform % [top bottom]))))) 
-  (java.util.ArrayList. (.size this)) this))) )   
+      (.add (normalise x #(transform % this))))) 
+  (java.util.ArrayList. (.size this)) this)) )   
   
 clojure.lang.IPersistentCollection;;if this fires, we don't know the type so we'll return a lazy-seq
 (normalise [this transform]
 (if (instance? java.util.Collection (first this))
 (map #(normalise % transform ) this)
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
-   (map (fn [x] (normalise x #(transform % [top bottom]))) this))) )
+  (map (fn [x] (normalise x #(transform % this))) this)) )
 
 clojure.lang.PersistentList
 (normalise
 [this transform]
 (if (instance? java.util.Collection (first this))
 (mapv #(normalise % transform) this)
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
-  (->> (mapv (fn [x] (normalise x #(transform % [top bottom]))) this)
+  (->> (mapv (fn [x] (normalise x #(transform % this))) this)
      rseq
-    (into '())))) ) 
+    (into '()))) ) 
     
 clojure.lang.LazySeq
 (normalise [this transform]
 (if (instance? java.util.Collection (first this))
 (map #(normalise % transform) this)
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
-   (map (fn [x] (normalise x #(transform % [top bottom]))) this))) )
+   (map (fn [x] (normalise x #(transform % this))) this)) )
       
 clojure.lang.IPersistentVector
 (normalise [this transform]
 (if (instance? java.util.Collection (first this))
 (mapv #(normalise % transform) this)
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
   (if (> 1124 (count this))     
-   (mapv (fn [x] (normalise x #(transform % [top bottom]))) this)
-   (into [] (r/foldcat (r/map (fn [x] (normalise x #(transform % [top bottom]))) this)))))) ) 
+   (mapv (fn [x] (normalise x #(transform % this))) this)
+   (into [] (r/foldcat (r/map (fn [x] (normalise x #(transform % this))) this))))) ) 
      
 clojure.lang.IPersistentSet ;;sets are typically not ordered so ordering will dissapear after processing
 (normalise [this transform]
 (if (instance? java.util.Collection (first this))
 (mapv #(normalise % transform) this)
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
  (persistent!        
-   (reduce (fn [ts x] (conj! ts (normalise x #(transform % [top bottom])))) (transient #{}) this)))) )
+   (reduce (fn [ts x] (conj! ts (normalise x #(transform % this)))) (transient #{}) this))) )
    
 clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND values (a dataset perhaps?)
 (normalise [this transform]
@@ -89,9 +78,7 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
 (extend-protocol Normalisable   
 (Class/forName "[D")  
 (normalise [this transform]
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
- (amap ^doubles this idx ret (double (normalise (aget ^doubles this idx) #(transform % [top bottom])))))))
+ (amap ^doubles this idx ret (double (normalise (aget ^doubles this idx) #(transform % this))))))
  
 (extend-protocol Normalisable   
 (Class/forName "[[D")  ;;2d double-arrays are very common
@@ -101,9 +88,7 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
 (extend-protocol Normalisable   
 (Class/forName "[F")  
 (normalise [this transform]
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
- (amap ^floats this idx ret (float (normalise (aget ^floats this idx) #(transform % [top bottom])))))))
+ (amap ^floats this idx ret (float (normalise (aget ^floats this idx) #(transform % this))))))
  
 (extend-protocol Normalisable   
 (Class/forName "[[F")  ;;2d float-arrays are very common
@@ -113,9 +98,7 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
 (extend-protocol Normalisable   
 (Class/forName "[J")  
 (normalise [this transform]
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
- (amap ^longs this idx ret (long (normalise (aget ^longs this idx) #(transform % [top bottom])))))))
+ (amap ^longs this idx ret (long (normalise (aget ^longs this idx) #(transform % this))))))
  
 (extend-protocol Normalisable   
 (Class/forName "[[J")  ;;2d long-arrays are very common
@@ -125,9 +108,7 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
 (extend-protocol Normalisable   
 (Class/forName "[I")  
 (normalise [this transform]
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
- (amap ^ints this idx ret (int (normalise (aget ^ints this idx) #(transform % [top bottom])))))))
+ (amap ^ints this idx ret (int (normalise (aget ^ints this idx) #(transform % this))))))
  
 (extend-protocol Normalisable   
 (Class/forName "[[I")  ;;2d int-arrays are also very common
@@ -137,9 +118,7 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
 (extend-protocol Normalisable   
 (Class/forName "[Ljava.lang.Object;")  
 (normalise [this transform]
- (let [top    (delay (apply max this))
-       bottom (delay (apply min this))]
- (amap #^"[Ljava.lang.Object;" this idx ret (normalise (aget #^"[Ljava.lang.Object;" this idx) #(transform % [top bottom]))))))
+ (amap #^"[Ljava.lang.Object;" this idx ret (normalise (aget #^"[Ljava.lang.Object;" this idx) #(transform % this)))))
  
 (extend-protocol Normalisable   
 (Class/forName "[Ljava.lang.String;")  
@@ -147,23 +126,26 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
  (amap #^"[Ljava.lang.String;" this idx ret (normalise (aget #^"[Ljava.lang.String;" this idx) #(transform % nil)))))  
    
    
-;;this is how client code would look like   
+;;this is how client code would look like
+(def in-range-needs  (memo/ttl (juxt #(apply max %) #(apply min %)) :ttl/threshold 3000)) ;the upper/lower data limits
+
+;;IN-RANGE formula  
 (defn in-range-formula 
 "The most common normalisation technique (for numbers)." 
-([x [tn bn] [ti bi]] 
+([x coll [bn tn]]  
+(let [[ti bi] (in-range-needs coll)]  
 (+ bn
-   (* (/ (- x (force bi)) 
-         (- (force ti) (force bi))) 
-      (- tn bn))) )
-([x [ti bi]] 
-  (in-range-formula x [1 -1] [ti bi]))
-([x] 
-  (in-range-formula x [1 -1] [10 -10])) )
+   (* (/ (- x  bi) 
+         (- ti bi)) 
+      (- tn bn))) ))
+([x coll] 
+  (in-range-formula x coll [-1 1])) )
 
 ;typical in-range transformers
-(def transform-in-range1 "In-range [1 -1] transformer." in-range-formula)
-(def transform-in-range5 "In-range [5 -5] transformer." #(in-range-formula %1 [5 -5] %2))
+(def transform-in-range1 "In-range [-1 1] transformer." in-range-formula)
+(def transform-in-range5 "In-range [-5 5] transformer." #(in-range-formula %1 %2 [-5 5]))
 
+;;RECIPROCAL formula
 (defn reciprocal-formula 
 "Reciprocal normalization is always normalizing to a number in the range between 0 and 1.
  It should only be used to normalize numbers greater than 1. In particular, do NOT pass in 0 as the first arg."
@@ -180,24 +162,59 @@ clojure.lang.IPersistentMap ;;assuming a map with collections for keys AND value
  
 (defn divide-by-value-formula
 "Normalises by dividing all elements by the given value." 
-([x div-val _]
+([x _ div-val]
    (/ x div-val))
 ([x _]
-  (divide-by-value-formula x 10 nil)) )       
+  (divide-by-value-formula x nil 10)) )       
 
-;typical divide-by-value transformer
-(def transform-by-value "Divide-by-value transformer."  divide-by-value-formula)
+;typical divide-by-value transformers
+(def transform-by-value10 "Divide-by-10 transformer."  divide-by-value-formula)
+(def transform-by-value2 "Divide-by-2 transformer."  #(divide-by-value-formula %1 %2 2))
 
 (defn porter-formula 
 "The normalisation 'formula' for Porter's algorithm." 
-([^String s lang-or-obj _]
+([^String s _ lang-or-obj]
   (help/porter-stem s lang-or-obj))
 ([^String s _] 
-  (porter-formula s "english" nil))
+  (porter-formula s  nil "english"))
 ([^String s] 
-  (porter-formula s "english")) )
+  (porter-formula s nil)) )
 
 ;typical porter transformers
 (def transform-by-porter "Porter's normalisation transformer for english." porter-formula)
-(def transform-by-porter-reuse "Porter's normalisation transformer that reuses the same object." #(porter-formula %1 (help/porter-stemmer "english") %2)) 
+(def transform-by-porter-reuse "Porter's normalisation transformer that reuses the same object." #(porter-formula %1 (help/porter-stemmer "english") %2))
+
+;MULTIPLICATIVE formula
+(defn multiplicative-needs* ^double [coll] 
+  (/ 1.0 (Math/sqrt (reduce #(+ % (* %2 %2)) coll)))) ;;produces the multiplicative normalisation factor
+  
+(def multiplicative-needs (memo/ttl multiplicative-needs* :ttl/threshold 3000)) ;;configure the caching threshold according to your needs 
+
+(defn multiplicative-formula 
+([x coll _]
+ (let [mfactor (multiplicative-needs coll)]
+    (* x mfactor)))
+([x coll]
+ (multiplicative-formula x coll nil) ))
+
+
+;Z-AXIS formula 
+(defn z-axis-needs* [coll]
+ (let [n (count coll)
+       vlength (Math/sqrt (reduce #(+ % (* %2 %2)) 0 coll)) 
+       zfactor (/ 1.0 (Math/sqrt n))
+       synthetic-field (* zfactor (Math/sqrt (- n (* vlength vlength))))]
+ [zfactor (if (or (Double/isInfinite synthetic-field)
+                  (Double/isNaN synthetic-field)) 0 synthetic-field)]))
+   
+(def z-axis-needs (memo/ttl z-axis-needs* :ttl/threshold 3000)) ;;configure the cashing threshold according to your needs 
+ 
+(defn z-axis-formula 
+([x coll _]
+ (let [[zfactor synthetic-field]  (z-axis-needs coll)] 
+  (+ synthetic-field (* x zfactor))))
+([x coll] 
+ (z-axis-formula x coll nil)) )     
+
+ 
    
