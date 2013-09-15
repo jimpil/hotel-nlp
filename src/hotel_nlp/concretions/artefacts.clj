84232b52cf2f8a05f76a0125ab918d2360378cda
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-09-16 00:02:04 +0100

    change defs to defonces

--- src/hotel_nlp/concretions/artefacts.clj
+++ src/hotel_nlp/concretions/artefacts.clj
@@ -12,18 +12,18 @@
 
 
 
-(def reg-tok (RE-Tokenizer. re/token-regex :string :string-seq))
-(def reg-seg (RE-Segmenter. re/sentence-segmentation-regex :string :string-seq))
-(def stick   (RE-Combiner. :string :string-seq))
-(def stemmer (PorterStemmer. "english" :string :string))
-(def bigrams  (Ngrams. 2 :string :string))
-(def MR-mapper  (MapReduceMapper. 5))
-(def main (Workflow. [reg-seg reg-tok stemmer]))
-(def alt  (Workflow. [reg-tok stemmer]))
-(def levenshtein (LevenshteinDistance. :strings :number))
-(def mr-mapper (MapReduceMapper. 100))
-(def brown-nltk-pos-probs (help/read-resource "corpora-train/BROWN-NLTK/brown-probs.txt"))
-(def pannotator-pluggable-sent-splitter  (Workflow. [reg-seg stick])) ;;replace reg-seg with your own sentence splitter
-(def abbreviation-extractor (RE-Abbrv. [re/re-abbreviation-paren re/re-term-paren] :string :map))
+(defonce reg-tok (RE-Tokenizer. re/token-regex :string :string-seq))
+(defonce reg-seg (RE-Segmenter. re/sentence-segmentation-regex :string :string-seq))
+(defonce stick   (RE-Combiner. :string :string-seq))
+(defonce stemmer (PorterStemmer. "english" :string :string))
+(defonce bigrams  (Ngrams. 2 :string :string))
+(defonce MR-mapper  (MapReduceMapper. 5))
+(defonce main (Workflow. [reg-seg reg-tok stemmer]))
+(defonce alt  (Workflow. [reg-tok stemmer]))
+(defonce levenshtein (LevenshteinDistance. :strings :number))
+(defonce mr-mapper (MapReduceMapper. 100))
+(defonce brown-nltk-pos-probs (help/read-resource "corpora-train/BROWN-NLTK/brown-probs.txt"))
+(defonce pannotator-pluggable-sent-splitter  (Workflow. [reg-seg stick])) ;;replace reg-seg with your own sentence splitter
+(defonce abbreviation-extractor (RE-Abbrv. [re/re-abbreviation-paren re/re-term-paren] :string :map))
 
 
