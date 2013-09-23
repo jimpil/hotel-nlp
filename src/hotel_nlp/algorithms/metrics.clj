d80da9b2b210063b2fff0d5d8059885974df9c44
Dimitrios Piliouras <jimpil1985@gmail.com>
2013-09-23 18:11:10 +0100

    added useful k-folds fn

--- src/hotel_nlp/algorithms/metrics.clj
+++ src/hotel_nlp/algorithms/metrics.clj
@@ -1,7 +1,8 @@
 (ns hotel_nlp.algorithms.metrics
   (:require  [clojure.core.reducers :as r] 
-             [clojure.test :refer [with-test is testing]])) 
-             
+             [clojure.test :refer [with-test is testing]]))
+              
+(set! *warn-on-reflection* true)             
 ;;pretty much what Edmund showed at the Conj!  :)           
 
 
@@ -25,7 +26,7 @@
 "Calculate the entropy for the given probability distribution."
 [ps] 
   (* -1 
-   (apply + (mapv entropy* ps))))
+   (reduce + (mapv entropy* ps))))
    
 (defn- entropy-asum
 "Calculate the entropy for the given probability distribution. Same as 'entropy-sum' but for arrays. " 
@@ -47,7 +48,7 @@
                  :or {using :reducers}}]
  (* -1
    (case using
-    :j-array   (entropy-asum (if (.isArray (.getClass distribution)) distribution (double-array distribution))) 
+    :j-array   (entropy-asum (if (.isArray ^Class (class distribution)) distribution (double-array distribution))) 
     :cl-vector (entropy-sum  (if (vector? distribution) distribution  (vec distribution))) 
     :reducers  (entropy-rsum (if (vector? distribution) distribution  (vec distribution))) 
       (throw (IllegalStateException. "Unsupported intermediate data-storage!")))))
@@ -69,33 +70,43 @@
       
 
 (defn confusion-matrix
-"Calculates the confusion matrix given all the elements (ground-truth) and all the predictions elements (predictions).
-The elements are expected to be in pairs of the type [element binary-class] -> [x :YES]  or [y :NO]. 
-Absence of prediction should be represented with [e :NO] or whatever other keyword you prefer via the :b-classes parameter.
-Also, the relevant and predictions element must be in the sctrict order they appeared or predictions, respectively."  
-[total predictions & {:keys [b-classes] 
-                      :or {b-classes [:YES :NO]}}] ;;positive class comes first
-(assert (= (count total) (count predictions)) "'total' & 'predictions' should have the same size!")                       
+"Calculates the confusion matrix, given all the elements (ground-truth) and all the predictions (should be of equal size).
+ The elements are expected to be in pairs of the type [element binary-class] -> [x :YES]  or [y :NO]. 
+ Absence of prediction should be represented with [e :NO] or whatever other keyword you prefer via the :b-classes parameter 
+ (the positive class should come first though!). Moreover, the g-truth and prediction elements must be in the sctrict order 
+ they appeared or predicted, respectively."  
+[g-truth predictions & {:keys [b-classes counts?] 
+                      :or {b-classes [:YES :NO] ;;positive class comes first!
+                           counts? false}}]  ;;want the seqs or the counts?
+(assert (= (count g-truth) 
+           (count predictions)) "'g-truth' & 'predictions' should have the same size!")                       
  (let [M (reduce-kv #(if (= %2 %3) 
                        (if (= (second %3) (first b-classes))             
                           (update-in % [:TRUE :positive] conj %3)
                           (update-in % [:TRUE :negative] conj %3)) 
                        (if (= (second %3) (first b-classes))             
                           (update-in % [:FALSE :positive] conj %3)
-                          (update-in % [:FALSE :negative] conj %3))) {:TRUE {:positive [] 
-                                                                             :negative []}
-                                                                      :FALSE {:positive [] 
-                                                                             :negative []}} (zipmap total predictions))]
- {:TP (get-in M [:TRUE :positive]) 
-  :TN (get-in M [:TRUE :negative])
-  :FP (get-in M [:FALSE :positive]) 
-  :FN (get-in M [:FALSE :negative])}))
+                          (update-in % [:FALSE :negative] conj %3))) 
+             {:TRUE {:positive [] 
+                     :negative []}
+              :FALSE {:positive [] 
+                      :negative []}} (zipmap g-truth predictions))]
+ {:TP ((if counts? count identity) 
+       (get-in M [:TRUE :positive])) 
+  :TN ((if counts? count identity) 
+       (get-in M [:TRUE :negative]))
+  :FP ((if counts? count identity) 
+       (get-in M [:FALSE :positive])) 
+  :FN ((if counts? count identity) 
+       (get-in M [:FALSE :negative]))}))
       
 (with-test      
 (defn IR-metrics 
 "Given a confusion-matrix (a regular map containing the keys :TP, :TN, :FP, :FN) calculates the most common IE metrics.
-The map can contain the raw numbers or the actual elementsin which case the seqs will be counted internally." 
+ The map can contain the raw numbers (in case you happen to know them in advance) or the actual elements, in which case the seqs will be counted internally." 
  [confusionM] ;;confusion-matrix (a regular map)
+ (assert (every? #(contains? confusionM %) '(:TP :TN :FP :FN)) 
+     "Some or none of the necessary keys [:TP :TN :FP :FN] were found in the confusion-map (matrix)! Aborting...")
   (let [tp (:TP confusionM) 
         tp (if (number? tp) tp (count tp))
         fp  (:FP confusionM)
@@ -113,14 +124,30 @@ The map can contain the raw numbers or the actual elementsin which case the seqs
              (/ (* P R) 
                 (+ P R)))] 
   {:precision P 
-   :recall R 
-   :fscore F1
+   :recall R   ;;sensitivity
+   :fscore F1  ;;balanced precision & recall
    :accuracy A
-   :specificity SP}))
+   :specificity SP})) ;;true negative-rate
    
 (testing "IR-metrics"
 (is (== 0.3   (:precision (IR-metrics {:TP 60 :FN 40 :TN 9760 :FP 140}))))
 (is (== 0.982 (:accuracy  (IR-metrics {:TP 60 :FN 40 :TN 9760 :FP 140}))))
-(is (== 0.6   (:recall (IR-metrics {:TP 60 :FN 40 :TN 9760 :FP 140}))))   ))   
-         
+(is (== 0.6   (:recall    (IR-metrics {:TP 60 :FN 40 :TN 9760 :FP 140}))))
+(is (thrown? AssertionError (:precision (IR-metrics {:TP 60 :FN 40 :TN 9760})))) ;;:FP is missing   
+))  
+
+
+(defn k-folds 
+"Partitions the given data into k partitions presumably to be used for cross-validation. 
+ Optional shuffling is supported but locked to false by lower arities. Use the 3-arg overload to override.
+ For simple 'holdout-validation' (2 partitions --> train/test), pass k = 2 or invoke using 
+ the single-parameter overload, passing only the data. Returns a lazy-seq." 
+([k data shuffle?]
+ (partition-all (/ (count data) k) ;;we need k partitions, NOT k elements in each partition
+   (cond-> data shuffle? shuffle)))
+([k data]
+ (k-folds k data false)) ;;don't suhffle by default
+([data]  ;;holdout validation
+ (k-folds 2 data)) ) 
+        
       
